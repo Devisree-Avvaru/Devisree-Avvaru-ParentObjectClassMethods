cf5d1cbda59ff2be6ab2e4ce1994fe75b9178e89
Devisree Avvaru <devisree.avvarus@gmail.com>
2025-03-12 18:33:51 +0000

    Completed Lab Parent Object Class Methods

--- src/main/java/SampleClass.java
+++ src/main/java/SampleClass.java
@@ -61,11 +61,22 @@
 public class SampleClass {
     public int a;
     public boolean b;
-
+    
+    public SampleClass(){}
+   
     //    implement a custom .equals(SampleClass other){} method here.
-
+    public boolean equals(Object obj){
+        if(this==obj) return true;
+        if(obj==null || getClass()!=obj.getClass()) return false;
+        SampleClass other = (SampleClass) obj;
+        return a == other.a && b == other.b; 
+    
+    }
 
     //    implement a custom .toString(){} method here.
-
+    public String toString() {
+        return "SampleClass{a=" + a + ", b=" + b + "}"; 
+    }
+    
     
 }
